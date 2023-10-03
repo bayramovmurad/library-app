8b0d8a178f85c1b2f7ffadb25b68f8e6f432efb6
huseyynov <ali.huseynov0905@gmail.com>
2023-10-03 11:19:47 +0400

    catalog is ready

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,3 +8,26 @@ document.getElementById('prev').onclick = function() {
     const widthItem = document.querySelector('.item').offsetWidth;
     document.getElementById('formList').scrollLeft -= widthItem;
 
+}
+
+
+document.getElementById('next2').onclick = function() {
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList2').scrollLeft += widthItem;
+}
+document.getElementById('prev2').onclick = function() {
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList2').scrollLeft -= widthItem;
+
+}
+
+
+document.getElementById('next3').onclick = function() {
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList3').scrollLeft += widthItem;
+}
+document.getElementById('prev3').onclick = function() {
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList3').scrollLeft -= widthItem;
+
+}
