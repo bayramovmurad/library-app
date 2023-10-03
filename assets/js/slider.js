fc50dd886c0cc10a6b14a9bc1010901e3d2749cd
huseyynov <ali.huseynov0905@gmail.com>
2023-10-03 13:10:55 +0400

    search slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,3 +9,16 @@ document.getElementById('prev').onclick = function() {
     document.getElementById('formList').scrollLeft -= widthItem;
 
 }
+
+
+
+
+document.getElementById('next-search').onclick = function() {
+    const widthItem = document.querySelector('.item-search').offsetWidth;
+    document.getElementById('formList-search').scrollLeft += widthItem;
+}
+document.getElementById('prev-search').onclick = function() {
+    const widthItem = document.querySelector('.item-search').offsetWidth;
+    document.getElementById('formList-search').scrollLeft -= widthItem;
+
+}
