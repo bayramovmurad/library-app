c2802384da7b42f7936ffe8f685d96e1dda12a13
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2023-10-03 11:23:42 +0400

    fic

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,3 +8,4 @@ document.getElementById('prev').onclick = function() {
     const widthItem = document.querySelector('.item').offsetWidth;
     document.getElementById('formList').scrollLeft -= widthItem;
 
+}
